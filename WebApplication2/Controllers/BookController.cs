9cbd2ae7fce90588c2b3d57f89eeea1d25cafea7
songtao1873 <479081675@qq.com%>
2023-12-19 09:00:11 +0800

    add_legacy code for mvc controller instead of context

--- WebApplication2/Controllers/BookController.cs
+++ WebApplication2/Controllers/BookController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using HH_XygPeixun;
+using NPOI.SS.Formula;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Web;
@@ -11,12 +15,12 @@ namespace WebApplication2.Controllers
     public class BookController : Controller
     {
         private ApplicationDbContext _context;
-
+        string strConn = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         public BookController()
         {
             _context = new ApplicationDbContext();
         }
-        // 释放资源
+        // release resource
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
@@ -53,65 +57,132 @@ namespace WebApplication2.Controllers
                 if (_context.Books.FirstOrDefault(b => b.Title == book.Title && b.Author == book.Author) != null)
                 {
                     ViewBag.ErrorMessage = "The user already exists.";
+                    // reset form
                     var viewModel = new BookFormViewModels
                     {
                         Book = book,
                     };
                     return View("BookForm", viewModel);
                 }
-                _context.Books.Add(book);
+                // legacy code: add
+                string sql = "INSERT INTO BookModels (Title, Author, Description) VALUES (@Title, @Author, @Description)";
+                SqlParameter[] sp = {
+                    new SqlParameter("@Title", book.Title),
+                    new SqlParameter("@Author", book.Author),
+                    new SqlParameter("@Description", book.Description)
+                };
+                RY_SQLHelper.getExecuteNonQuery(strConn, sql, sp);
+                // MVC code
+                //_context.Books.Add(book);
             }
             // Edit Book
             else
             {
-                var bookInDb = _context.Books.Single(c => c.Id == book.Id);
-                bookInDb.Author = book.Author;
-                bookInDb.Description = book.Description;
+                // legacy code: update
+                string sql = "UPDATE BookModels SET Title = @Title, Author = @Author, Description = @Description where Id=@Id";
+                SqlParameter[] sp = { 
+                    new SqlParameter("@Id", book.Id.ToString()),
+                    new SqlParameter("@Title", book.Title),
+                    new SqlParameter("@Author", book.Author),
+                    new SqlParameter("@Description", book.Description)
+                };
+                RY_SQLHelper.getExecuteNonQuery(strConn, sql, sp);
+                //var bookInDb = _context.Books.Single(c => c.Id == book.Id);
+                //bookInDb.Author = book.Author;
+                //bookInDb.Description = book.Description;
             }
-            _context.SaveChanges();
+            //_context.SaveChanges();
             return RedirectToAction("Index", "Book");
         }
 
         public ViewResult Index()
         {
-            var books = _context.Books.ToList();
+            // legacy code: query
+            string sql = "SELECT * FROM BookModels";
+            DataTable dataTable = RY_SQLHelper.getDataTable(strConn, 0, sql);
+            List<BookModels> books = dataTable.AsEnumerable()
+            .Select(row => new BookModels
+            {
+                Id = Convert.ToInt32(row["Id"]),
+                Title = Convert.ToString(row["Title"]),
+                Author = Convert.ToString(row["Author"]),
+                Description = Convert.ToString(row["Description"]),
+            }).ToList();
             return View(books);
+
+            // MVC code
+            //var books = _context.Books.ToList();
+            //return View(books);
         }
 
         [Authorize]
         public ActionResult Edit(int id)
         {
-            var book = _context.Books.SingleOrDefault(b => b.Id == id);
-            if (book == null)
+            //legacy code: query
+            string sql = $"SELECT * FROM BookModels WHERE ID = {id.ToString()}";
+            DataTable dataTable = RY_SQLHelper.getDataTable(strConn, 0, sql);
+            List<BookModels> books = dataTable.AsEnumerable()
+            .Select(row => new BookModels
             {
-                return HttpNotFound();
+                Id = Convert.ToInt32(row["Id"]),
+                Title = Convert.ToString(row["Title"]),
+                Author = Convert.ToString(row["Author"]),
+                Description = Convert.ToString(row["Description"]),
+            }).ToList();
+            if (books.Count == 1)
+            {
+                var bookModel = books[0];
+                var viewModel = new BookFormViewModels
+                {
+                    Book = bookModel
+                };
+                return View("BookForm", viewModel);
+
             }
-            var viewModel = new BookFormViewModels
+            else
             {
-                Book = book,
-                //MembershipTypes = _context.MembershipTypes.ToList()
+                ViewBag.ErrorMessage = $"ID: {id},有毛病";
+                return View();
             };
-            return View("BookForm", viewModel);
+
+            // MVC code
+            //var book = _context.Books.SingleOrDefault(b => b.Id == id);
+            //if (book == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //var viewModel = new BookFormViewModels
+            //{
+            //    Book = book,
+            //};
+            //return View("BookForm", viewModel);
         }
 
         [Authorize]
         public ActionResult Delete(int id)
         {
-            // Retrieve the book from the database
-            var book = _context.Books.Find(id);
-
-            // Check if the book exists
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-
-            // Remove the book from the context and save changes
-            _context.Books.Remove(book);
-            _context.SaveChanges();
-
-            // Redirect to the index action (or any other desired action)
+            // legacy code: delete
+            string sql = "DELETE FROM BookModels where Id=@Id";
+            SqlParameter[] sp = { new SqlParameter("@Id", id.ToString()) };
+            RY_SQLHelper.getExecuteNonQuery(strConn, sql, sp);
             return RedirectToAction("Index", "Book");
+            // MVC code
+            //// Find out the book from the database
+            //var book = _context.Books.Find(id);
+
+            //// Check if the book exists
+            //if (book == null)
+            //{
+            //    return HttpNotFound();
+            //}
+
+            //// Remove the book from the context and save changes
+            //_context.Books.Remove(book);
+            //_context.SaveChanges();
+
+            //// Redirect to the index action (or any other desired action)
+            //return RedirectToAction("Index", "Book");
         }
+
     }
 }
---
+++ WebApplication2/Migrations/202312190026306_add_content.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_content : DbMigration
+    {
+        public override void Up()
+        {
+            Sql();
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
